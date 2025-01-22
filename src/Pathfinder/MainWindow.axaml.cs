720a2209e5721f0b1ca152330e726335a694c8c7
niko <niko.summanen1@gmail.com>
2025-01-23 00:14:44 +0200

    Paranna visualisointia

--- src/Pathfinder/MainWindow.axaml.cs
+++ src/Pathfinder/MainWindow.axaml.cs
@@ -11,53 +11,22 @@ using Avalonia;
 using Avalonia.Platform;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Collections;
+using System.Security.Cryptography;
+using System.ComponentModel;
+using Avalonia.Input;
 
 namespace Pathfinder;
 
 public partial class MainWindow : Window
 {
     private WriteableBitmap _bitmap;
+    private Point _startPosition;
+    public static bool ShouldCallCallback = true;
 
     public MainWindow()
     {
         InitializeComponent();
-
-        WarmupAlgorithms();
-    }
-
-    private async Task WarmupAlgorithms()
-    {
-        StatusTextBox.Text = "Warming up algorithms";
-
-        var algorithms = new IPathFindingAlgorithm[] { new BFS(), new AStar() };
-
-        await Task.Run(() => 
-        {
-            var rnd = new Random();
-
-            for (int i = 0; i < 10000; i++)
-            {
-                var map = new int[100, 100];
-                for (int y = 0; y < map.GetLength(1); y++)
-                {
-                    for (int x = 0; x < map.GetLength(0); x++)
-                    {
-                        map[x, y] = rnd.Next(0, 2);
-                    }
-                }
-
-                var start = new Node(rnd.Next(0, map.GetLength(0)), rnd.Next(0, map.GetLength(1)));
-                var goal = new Node(rnd.Next(0, map.GetLength(0)), rnd.Next(0, map.GetLength(1)));
-
-                foreach (var algorithm in algorithms)
-                {
-                    var path = algorithm.Search(map, start, goal, null, 0, TimeSpan.Zero);
-                    Console.WriteLine(path.Count);
-                }
-            }
-        });
-
-        StatusTextBox.Text = "Ready";
     }
 
     private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
@@ -65,9 +34,46 @@ public partial class MainWindow : Window
         StartVisualization();
     }
 
+    private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(VisualizationImage).Properties.IsLeftButtonPressed)
+        {
+            _startPosition = e.GetPosition(VisualizationImage);
+            StartTextBox.Text = $"{(int)_startPosition.X},{(int)_startPosition.Y}";
+        }
+    }
+
+    private void OnPointerMoved(object? sender, PointerEventArgs e)
+    {
+        if (e.GetCurrentPoint(VisualizationImage).Properties.IsLeftButtonPressed)
+        {
+            Point currentPosition = e.GetPosition(VisualizationImage);
+            UpdateLine(_startPosition, currentPosition);
+        }
+    }
+
+    private void OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        if (e.InitialPressMouseButton == MouseButton.Left)
+        {
+            var goalPosition = e.GetPosition(VisualizationImage);
+            GoalTextBox.Text = $"{(int)goalPosition.X},{(int)goalPosition.Y}";
+        }
+    }
+
+    private void UpdateLine(Point start, Point end)
+    {
+        DynamicLine.StartPoint = start;
+        DynamicLine.EndPoint = end;
+        DynamicLine.IsVisible = true;
+    }
+
     public async void StartVisualization()
     {
+        DynamicLine.IsVisible = false;
+
         var map = Input.ReadMapFromFile(MapTextBox.Text);
+        //var map = Input.ReadMapFromImage(MapTextBox.Text);
 
         _bitmap = new WriteableBitmap(new PixelSize(map.GetLength(0), map.GetLength(1)), new Vector(96, 96), PixelFormat.Rgba8888);
         VisualizationImage.Source = _bitmap;
@@ -80,39 +86,51 @@ public partial class MainWindow : Window
         var goalNumbers = GoalTextBox.Text.Split(',');
         var goal = new Node(int.Parse(goalNumbers[0]), int.Parse(goalNumbers[1]));
 
+        var allowDiagonal = AllowDiagonalCheckBox.IsChecked.HasValue && AllowDiagonalCheckBox.IsChecked.Value;
+
         IPathFindingAlgorithm algorithm = AlgorithmComboBox.SelectedIndex switch
         {
-            0 => new BFS(),
-            1 => new AStar(),
+            0 => new Dijkstra(map),
+            1 => new AStar(map),
             _ => throw new NotImplementedException()
         };
 
         var sw = new Stopwatch();
 
-        if (VisualizeRadioButton.IsChecked.HasValue && VisualizeRadioButton.IsChecked.Value)
+        var stepDelay = StepDelaySlider.Value;
+
+        PathFindingResult result;
+
+        if (stepDelay > 0)
         {
+            var stepDelayTimeSpan = TimeSpan.FromMicroseconds(Math.Pow(stepDelay, 4));
             sw.Start();
-            await Task.Run(() => algorithm.Search(map, start, goal, Callback, (int)Math.Pow(2, 6), TimeSpan.FromMicroseconds(1000)));
+            result = await Task.Run(() => algorithm.Search(start, goal, allowDiagonal, Callback, stepDelayTimeSpan));
             sw.Stop();
         }
         else
         {
             sw.Start();
-            var path = algorithm.Search(map, start, goal, null, 0, TimeSpan.Zero);
+            result = algorithm.Search(start, goal, allowDiagonal);
             sw.Stop();
-
-            DrawMap(map, new List<Node>(), new List<Node>(), new Node(0, 0), path);
         }
+
+        var visited = result.VisitedNodes;
+        IEnumerable<Node> emptyList = new List<Node>();
+
+        DrawMap(map, ref visited, ref emptyList, new Node(0, 0), result.Path);
 
         TimeTakenTextBox.Text = $"{sw.Elapsed.TotalMilliseconds} ms";
     }
 
-    private void Callback(int[,] map, ICollection<Node> visited, ICollection<Node> queue, Node current, ICollection<Node>? path)
+    private void Callback(int[,] map, IEnumerable<Node> visited, IEnumerable<Node> queue, Node current)
     {
+        ShouldCallCallback = false;
+
         Dispatcher.UIThread.InvokeAsync(() =>
         {
-            NodesVisitedTextBox.Text = visited.Count.ToString();
-            DrawMap(map, visited, queue, current, path);
+            DrawMap(map, ref visited, ref queue, current, null);
+            ShouldCallCallback = true;
         });
     }
 
@@ -142,8 +160,10 @@ public partial class MainWindow : Window
         VisualizationImage.InvalidateVisual();
     }
 
-    private void DrawMap(int[,] map, ICollection<Node> visited, ICollection<Node> queue, Node current, ICollection<Node>? path)
+    private void DrawMap(int[,] map, ref IEnumerable<Node> visited, ref IEnumerable<Node> queue, Node? current, ICollection<Node>? path)
     {
+        var visitedCount = 0;
+
         using (var frameBuffer = _bitmap.Lock())
         {
             unsafe
@@ -153,12 +173,15 @@ public partial class MainWindow : Window
 
                 foreach (var node in visited)
                 {
+                    visitedCount++;
                     buffer[node.Y * stride + node.X] = Brushes.LightGreen.Color.ToUInt32();
                 }
+
                 foreach (var node in queue)
                 {
                     buffer[node.Y * stride + node.X] = Brushes.LightBlue.Color.ToUInt32();
                 }
+                
                 if (path != null)
                 {
                     foreach (var node in path)
@@ -167,10 +190,15 @@ public partial class MainWindow : Window
                     }
                 }
                 
-                buffer[current.Y * stride + current.X] = Brushes.Red.Color.ToUInt32();
+                if (current != null)
+                {
+                    buffer[current.Value.Y * stride + current.Value.X] = Brushes.Red.Color.ToUInt32();
+                }
             }
         }
 
         VisualizationImage.InvalidateVisual();
+
+        NodesVisitedTextBox.Text = visitedCount.ToString();
     }
 }
