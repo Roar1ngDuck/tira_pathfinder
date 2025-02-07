d6b83f12b89b078ee660e8d1f9d90c888c26fc55
niko <niko.summanen1@gmail.com>
2025-02-07 16:24:42 +0200

    Lisää parempi nopeuden hallinta

---
+++ src/Pathfinder/StepDelay.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pathfinder
+{
+    public class StepDelay
+    {
+        Stopwatch _timingStopwatch;
+        long _timingNodeCounter;
+        private TimeSpan _targetStepDelay;
+
+        public StepDelay(TimeSpan targetStepDelay)
+        {
+            _targetStepDelay = targetStepDelay;
+            _timingStopwatch = Stopwatch.StartNew();
+            _timingNodeCounter = 0;
+        }
+
+        public void UpdateTargetStepDelay(TimeSpan targetStepDelay)
+        {
+            _targetStepDelay = targetStepDelay;
+            _timingStopwatch = Stopwatch.StartNew();
+            _timingNodeCounter = 0;
+        }
+
+        public void Wait()
+        {
+            double elapsedMs = _timingStopwatch.Elapsed.TotalMilliseconds;
+            double targetDelay = _timingNodeCounter * _targetStepDelay.TotalMilliseconds;
+            if (elapsedMs < targetDelay)
+            {
+                Thread.Sleep((int)Math.Max(1, targetDelay - elapsedMs));
+            }
+
+            _timingNodeCounter++;
+        }
+    }
+}
